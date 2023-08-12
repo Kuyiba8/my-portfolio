77625db73690c2db121894c03450f329df35b8c4
Kuyiba Anthony <kuyibanthony88@gmail.com>
2023-08-12 21:31:50 +0000

    modified Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import myProfile from "./Images/profile.jpg";
+import tony from "./Images/tony.jpg";
 import { GoArrowRight } from "react-icons/go";
 import { Link } from "react-scroll";
 
@@ -8,10 +8,10 @@ const Home = () => {
     <div name="home" className="h-full pt-20 w-full bg-slate-200 ">
       <div className="w-full mx-auto flex flex-col items-center justify-evenly h-full px-2 sm:flex-row">
         <div className="flex flex-col justify-center ">
-          <h2 className="text-3xl sm:text-5xl font-bold">
+          <h2 className="md:ml-20 text-3xl sm:text-5xl font-bold">
             I'm a Full Stack Developer
           </h2>
-          <p className="text-xl py-4 w-full">
+          <p className="text-xl py-4 md:ml-20 ">
             I have one year of experience building and designing software.
             Currently, i love to work on web applcation using technologies like
             React, Tailwindcss and Next JS.
@@ -21,7 +21,7 @@ const Home = () => {
               to="portfolio"
               smooth
               duration={500}
-              className="group p-2 w-2/5 border rounded-2xl bg-green-400 cursor-pointer flex items-center"
+              className="group p-2 w-2/5 md:ml-20 border rounded-2xl bg-green-400 cursor-pointer flex items-center"
             >
               Portfolio
               <span className="group-hover:rotate-90 duration-300">
@@ -30,10 +30,10 @@ const Home = () => {
             </Link>
           </div>
         </div>
-        <div className="sm:mt-20 py-4 h-auto sm:w-2/5 lg:w-2/6">
+        <div className="sm:mt-20 py-4 h-auto sm:w-3/4 lg:w-3/6">
           <img
-            className=" w-2/4 sm:w-full md:w-2/4 rounded-full mx-auto"
-            src={myProfile}
+            className=" w-2/4 sm:w-full md:w-3/4 rounded-full hover:scale-110 mx-auto"
+            src={tony}
             alt="my profile"
           />
         </div>
