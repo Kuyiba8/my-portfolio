bebe9c6ff28e2966e06e1c84b884a2ee57e69b5d
Kuyiba Anthony <kuyibanthony88@gmail.com>
2023-08-13 14:02:49 +0000

    modified portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -10,22 +10,27 @@ const Portfolio = () => {
     {
       id: 1,
       src: html,
+      href: "https://www.sololearn.com/profile/20793231",
     },
     {
       id: 2,
       src: css,
+      href: "https://www.sololearn.com/profile/20793231",
     },
     {
       id: 3,
       src: game,
+      href: "https://www.sololearn.com/profile/20793231",
     },
     {
       id: 4,
       src: web,
+      href: "https://www.sololearn.com/profile/20793231",
     },
     {
       id: 5,
       src: marketers,
+      href: "https://www.sololearn.com/profile/20793231",
     },
   ];
 
@@ -40,17 +45,19 @@ const Portfolio = () => {
             Portfolio
           </p>
           <p className="text-center text-xl py-6">
-            Check out some of my achievement right here
+            Track my progress and Check out some of my achievements
           </p>
         </div>
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src }) => (
+          {portfolios.map(({ id, src, href }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
-              <img
-                src={src}
-                className="rounded-md duration-200 hover:scale-105"
-                alt=""
-              />
+              <a href={href} target="_blank" rel="noreferrer">
+                <img
+                  src={src}
+                  className="rounded-md duration-200 hover:scale-105"
+                  alt=""
+                />
+              </a>
               <div className="flex justify-center items-center">
                 <button className="w-full px-2 py-3 m-2 duration-200 hover:scale-105">
                   CERTIFICATE
